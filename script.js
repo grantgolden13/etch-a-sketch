7b76ff65bc8b5a56017b54884cd53db393267c5c
grantgolden13 <grantgolden13@gmail.com>
2021-10-13 16:46:23 -0700

    style body and add and style container w/ JS

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.createElement('div');
+let body = document.querySelector('body');
+
+body.style = "display: flex; justify-content: center; align-items: center; height: 100vh;"
+container.style = "border: 2px solid black; height: 80vh; width: 80vw";
+
+body.appendChild(container);
