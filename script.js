1b9a9fdb045632bad78fc7d7e23d95bd76fcdd6d
grantgolden13 <grantgolden13@gmail.com>
2021-10-25 12:44:00 -0700

    finish grid functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const container = document.querySelector('#grid');
 
-// make new divs
+// make new divs and add event listeners to each for drawing functionality
 
 function makeCell (gridSize) {
     for (let i = 0; i < gridSize; i++) {
@@ -10,24 +10,34 @@ function makeCell (gridSize) {
         newCell.classList.add('newCells');
         container.appendChild(newCell);
     }
+    let cells = document.querySelectorAll('.newCells');
+    cells.forEach(div => {
+        div.addEventListener('mouseover', function mouseOver(e) {
+            e.target.style = "background-color: black";
+        });
+    });
 }
 
-// add drawing functionality w/ event listeners
+// make grid
 
-const divs = document.querySelectorAll('.newCells');
-divs.forEach(div => {
-    div.addEventListener('mouseover', function mouseOver(e) {
-        e.target.style = "background-color: black";
-    });
-});
+function makeGrid(numRows, numColumns) {
+    container.style.setProperty('--numRows', numRows);
+    container.style.setProperty('--numColumns', numColumns);
+    for (i = 0; i < numRows; i++) {
+        makeCell(numRows);
+    }
+}
 
-// makes grid
+// prompt user for # of grid cells per side, pass that value thru makeGrid()
 
-function makeGrid() {
-    let sides = prompt("How many grid squares per side? (16-100)", 24);
-    console.log(sides);
-    
-    return sides;
+function promptNewGrid() {
+    let number = prompt("How many cells per side? (16-72)", 64);
+    if (number >= 16 && number <= 72) {
+        makeGrid(number, number);
+    } else {
+        number = prompt("Invalid number! Pick a number between 16 and 72.", 64);
+        makeGrid(number, number);
+    }
 }
 
 // reset button
@@ -37,4 +47,4 @@ resetButton.addEventListener('click', function () {
         window.location.reload();
 });
 
-makeGrid();
+promptNewGrid();
